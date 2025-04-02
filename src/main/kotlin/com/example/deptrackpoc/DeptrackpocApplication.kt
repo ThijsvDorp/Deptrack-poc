b87e57a57293e29b2eb0cf0df8a7a846351ec938
Thijs <thijsvdorp@hotmail.com>
2025-04-02 13:56:33 +0200

    initialize Spring Boot project

---
+++ src/main/kotlin/com/example/deptrackpoc/DeptrackpocApplication.kt
@@ -0,0 +1,11 @@
+package com.example.deptrackpoc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeptrackpocApplication
+
+fun main(args: Array<String>) {
+	runApplication<DeptrackpocApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/deptrackpoc/DeptrackpocApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.deptrackpoc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeptrackpocApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
